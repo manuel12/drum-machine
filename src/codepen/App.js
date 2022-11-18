0e36d5aa9bc6d33e5ffdb4151c90d75a7d41b020
Manuel <manuelpinedacabeza@gmail.com>
2022-11-18 12:59:21 +0100

    Added drum-pad-button-pressed on props.keyPressed conditional.

--- src/codepen/App.js
+++ src/codepen/App.js
@@ -1,7 +1,9 @@
 const DrumPadButton = (props) => {
   return (
     <div
-      className={`drum-pad drum-pad-button button col-md`}
+      className={`drum-pad drum-pad-button button ${
+        props.keyPressed && "drum-pad-button-pressed"
+      } col-md`}
       id={props.sampleName}
       onClick={props.playSound}
     >
@@ -225,7 +227,7 @@ function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
